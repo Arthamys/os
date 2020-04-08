135817fef6fe24ac26c66984237d75408e6e1b91
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-04-08 23:28:06 +0200

    Factor out code into a separate `os` library

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-
 #![no_std]
 #![no_main]
-
+#![feature(custom_test_frameworks)]
+#![test_runner(os::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-mod vga_buffer;
-mod serial;
+use os::println;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
@@ -16,6 +13,7 @@ pub extern "C" fn _start() -> ! {
 
     #[cfg(test)]
     test_main();
+
     loop {}
 }
 
@@ -29,35 +27,5 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        // Use the port specified as the `isa-debug-exit` port in qemu config
-        let mut port = Port::new(0xf4);
-        // Write our exit code to the port
-        port.write(exit_code as u32);
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Success);
+    os::test_panic_handler(info);
 }
