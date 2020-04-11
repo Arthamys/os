c25c456988981d3141d22dd175e03d609c952fc5
Galilée 'Bill' Enguehard <galilee.enguehard@gmail.com>
2020-04-11 17:06:51 +0200

    Reload code segment and TSS upon GDT init
    The GDT is not correctly initialized until we update the segments we
    have just created in the CPU.
    To do that, we now keep track of the different selectors that we have
    created in our GDT through the Selectors structure, and, when
    initializing the GDT, we call the necessary instructions to reload the
    segments.

--- src/gdt.rs
+++ src/gdt.rs
@@ -3,18 +3,24 @@ use x86_64::structures::{
     tss::TaskStateSegment,
     gdt::GlobalDescriptorTable,
     gdt::Descriptor,
+    gdt::SegmentSelector,
 };
 use lazy_static::lazy_static;
 
 pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
 
 lazy_static! {
-    static ref GDT: GlobalDescriptorTable = {
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
         let mut gdt = GlobalDescriptorTable::new();
-        gdt.add_entry(Descriptor::kernel_code_segment());
-        gdt.add_entry(Descriptor::tss_segment(&TSS));
-        gdt
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (gdt, Selectors { code_selector, tss_selector })
     };
+}
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
 }
 
 lazy_static! {
@@ -33,5 +39,13 @@ lazy_static! {
 }
 
 pub fn init() {
-    GDT.load();
+    use x86_64::instructions::{segmentation::set_cs, tables::load_tss};
+    GDT.0.load();
+    // These operations are unsafe as they may break memory safety if we load invalid sectors
+    unsafe {
+        // reload the code selector
+        set_cs(GDT.1.code_selector);
+        // load the new TSS
+        load_tss(GDT.1.tss_selector);
+    }
 }
